2431b3e6bc182ad65390faa98d1db948f6a77703
Gabriel Araujo <51542095+gabrujo@users.noreply.github.com>
2021-03-09 16:51:08 -0500

    Pharmacy side code

---
+++ PharmacyDB/pharmagather_db.sql
@@ -0,0 +1,61 @@
+CREATE TABLE costumer_info
+ADD ROW First_Name
+ADD ROW Last_Name
+ADD ROW DOB
+ADD ROW Cell
+ADD ROW Hook_Num
+ADD ROW Address;
+
+ALTER TABLE costumer_info
+ADD COLUMN First_Name = Prashanna
+ADD COLUMN First_Name = Madhi
+ADD COLUMN First_Name = Gabriel
+ADD COLUMN First_Name = Fathia
+ADD COLUMN First_Name = Rajvir
+ADD COLUMN First_Name = Jhon
+ADD COLUMN First_Name = James;
+
+ALTER TABLE costumer_info
+ADD COLUMN Last_Name = Kandel
+ADD COLUMN Last_Name = Saeikhalili
+ADD COLUMN Last_Name = Araujo
+ADD COLUMN Last_Name = Ajallem
+ADD COLUMN Last_Name = Kel
+ADD COLUMN Last_Name = Smith
+ADD COLUMN Last_Name = Bond;
+
+ALTER TABLE costumer_info
+ADD COLUMN DOB = 7/25/1998
+ADD COLUMN DOB = 1/27/1978
+ADD COLUMN DOB = 2/14/1998
+ADD COLUMN DOB = 3/29/1978
+ADD COLUMN DOB = 7/25/1988
+ADD COLUMN DOB = 9/2/1978
+ADD COLUMN DOB = 10/22/1958;
+
+ALTER TABLE costumer_info
+ADD COLUMN Cell = 203-305-5140
+ADD COLUMN Cell = 203-446-6750
+ADD COLUMN Cell = 203-556-9245
+ADD COLUMN Cell = 203-572-4023
+ADD COLUMN Cell = 203-278-4926
+ADD COLUMN Cell = 203-439-3667
+ADD COLUMN Cell = 203-007-9369;
+
+ALTER TABLE costumer_info
+ADD COLUMN Hook_Num = 1
+ADD COLUMN Hook_Num = 2
+ADD COLUMN Hook_Num = 3
+ADD COLUMN Hook_Num = 4
+ADD COLUMN Hook_Num = 5
+ADD COLUMN Hook_Num = 6
+ADD COLUMN Hook_Num = 7;
+
+ALTER TABLE costumer_info
+ADD COLUMN Address = 126 Park Avenue, Bridgeport, CT, 06604
+ADD COLUMN Address = 126 Park Avenue, Bridgeport, CT, 06604
+ADD COLUMN Address = 126 Park Avenue, Bridgeport, CT, 06604
+ADD COLUMN Address = 126 Park Avenue, Bridgeport, CT, 06604
+ADD COLUMN Address = 126 Park Avenue, Bridgeport, CT, 06604
+ADD COLUMN Address = 126 Park Avenue, Bridgeport, CT, 06604
+ADD COLUMN Address = 126 Park Avenue, Bridgeport, CT, 06604;
